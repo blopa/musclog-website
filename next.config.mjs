bcdcfb750aef9f4296ae72c74a8fc83579a0e24e
Pablo Pirata <pablo@studocu.com>
2024-07-14 03:59:42 +0200

    ookko

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = "/musclog-website";
+
 const nextConfig = {
     output: 'export',
-    basePath: "/musclog-website",
+    basePath,
     reactStrictMode: true,
     async rewrites() {
         return [
             {
                 source: '/locales/en-US/translation.json',
-                destination: '/musclog-website/locales/en-US/translation.json',
+                destination: `${basePath}/locales/en-US/translation.json`,
             },
             {
                 source: '/locales/pt-BR/translation.json',
-                destination: '/musclog-website/locales/pt-BR/translation.json',
+                destination: `${basePath}/locales/pt-BR/translation.json`,
             },
             {
                 source: '/locales/nl-NL/translation.json',
-                destination: '/musclog-website/locales/nl-NL/translation.json',
+                destination: `${basePath}/locales/nl-NL/translation.json`,
             },
             {
                 source: '/locales/es-ES/translation.json',
-                destination: '/musclog-website/locales/es-ES/translation.json',
+                destination: `${basePath}/locales/es-ES/translation.json`,
             },
         ]
     },
     // i18n: {
-    //     locales: ['en-US', 'pt-BR', 'nl-NL'],
+    //     locales: ['en-US', 'pt-BR', 'nl-NL', 'es-ES'],
     //     defaultLocale: 'en-US',
     // },
 };
