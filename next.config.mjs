682bda0c0c5c57be2b2a84e36dc2ca3b992edea4
Pablo Pirata <pablo@studocu.com>
2024-07-14 03:38:05 +0200

    ok again

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ['en-US', 'pt-BR', 'nl-NL'],
-        defaultLocale: 'en-US',
-    },
+    output: 'export',
+    // i18n: {
+    //     locales: ['en-US', 'pt-BR', 'nl-NL'],
+    //     defaultLocale: 'en-US',
+    // },
 };
 
 export default nextConfig;
