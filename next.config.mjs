7d2fc17c2c53887b8d3404da75cbf81b2e4b6a59
Pablo Pirata <pablo@studocu.com>
2024-07-14 03:55:15 +0200

    try rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ const nextConfig = {
     output: 'export',
     basePath: "/musclog-website",
     reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/locales/en-US/translation.json',
+                destination: '/musclog-website/locales/en-US/translation.json',
+            },
+            {
+                source: '/locales/pt-BR/translation.json',
+                destination: '/musclog-website/locales/pt-BR/translation.json',
+            },
+            {
+                source: '/locales/nl-NL/translation.json',
+                destination: '/musclog-website/locales/nl-NL/translation.json',
+            },
+            {
+                source: '/locales/es-ES/translation.json',
+                destination: '/musclog-website/locales/es-ES/translation.json',
+            },
+        ]
+    },
     // i18n: {
     //     locales: ['en-US', 'pt-BR', 'nl-NL'],
     //     defaultLocale: 'en-US',
