09674d7a243a80ae24e71d5835bec9d3fee7275f
Pablo Pirata <pablo@studocu.com>
2024-07-14 04:06:16 +0200

    ookko23

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,30 @@
 /** @type {import('next').NextConfig} */
-
-const basePath = "/musclog-website";
-
 const nextConfig = {
+    basePath: '/musclog-website',
     output: 'export',
-    basePath,
     reactStrictMode: true,
     async rewrites() {
         return {
             beforeFiles: [
                 {
+                    destination:
+                        '/musclog-website/locales/en-US/translation.json',
                     source: '/locales/en-US/translation.json',
-                    destination:
-                        `${basePath}/locales/en-US/translation.json`,
                 },
                 {
+                    destination:
+                        '/musclog-website/locales/pt-BR/translation.json',
                     source: '/locales/pt-BR/translation.json',
-                    destination:
-                        `${basePath}/locales/pt-BR/translation.json`,
                 },
                 {
+                    destination:
+                        '/musclog-website/locales/nl-NL/translation.json',
                     source: '/locales/nl-NL/translation.json',
-                    destination:
-                        `${basePath}/locales/nl-NL/translation.json`,
                 },
                 {
-                    source: '/locales/es-ES/translation.json',
                     destination:
-                        `${basePath}/locales/es-ES/translation.json`,
+                        '/musclog-website/locales/es-ES/translation.json',
+                    source: '/locales/es-ES/translation.json',
                 },
             ]
         }
