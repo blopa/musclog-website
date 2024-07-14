227410dcaf7d5a3d6bf56dd52ecb4171b1b493fa
Pablo Pirata <pablo@studocu.com>
2024-07-14 04:03:11 +0200

    ookko2

--- next.config.mjs
+++ next.config.mjs
@@ -7,24 +7,30 @@ const nextConfig = {
     basePath,
     reactStrictMode: true,
     async rewrites() {
-        return [
-            {
-                source: '/locales/en-US/translation.json',
-                destination: `${basePath}/locales/en-US/translation.json`,
-            },
-            {
-                source: '/locales/pt-BR/translation.json',
-                destination: `${basePath}/locales/pt-BR/translation.json`,
-            },
-            {
-                source: '/locales/nl-NL/translation.json',
-                destination: `${basePath}/locales/nl-NL/translation.json`,
-            },
-            {
-                source: '/locales/es-ES/translation.json',
-                destination: `${basePath}/locales/es-ES/translation.json`,
-            },
-        ]
+        return {
+            beforeFiles: [
+                {
+                    source: '/locales/en-US/translation.json',
+                    destination:
+                        `${basePath}/locales/en-US/translation.json`,
+                },
+                {
+                    source: '/locales/pt-BR/translation.json',
+                    destination:
+                        `${basePath}/locales/pt-BR/translation.json`,
+                },
+                {
+                    source: '/locales/nl-NL/translation.json',
+                    destination:
+                        `${basePath}/locales/nl-NL/translation.json`,
+                },
+                {
+                    source: '/locales/es-ES/translation.json',
+                    destination:
+                        `${basePath}/locales/es-ES/translation.json`,
+                },
+            ]
+        }
     },
     // i18n: {
     //     locales: ['en-US', 'pt-BR', 'nl-NL', 'es-ES'],
